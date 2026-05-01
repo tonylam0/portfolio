d52e6400cbfab269354f29e0c8f4f20997e145b7
Tonycs <lam.tony540@gmail.com>
2026-05-01 12:42:21 -0400

    Changed website title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils";
 const geist = Geist({ subsets: ['latin'], variable: '--font-sans' });
 
 export const metadata: Metadata = {
-  title: "Tony | Portfolio",
+  title: "tony lam",
   description: "Tony's portfolio",
 };
 
